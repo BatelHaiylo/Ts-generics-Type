e81cb80076f475b5dbfb717b7ecfe1f80203fdd1
Batel Haiylo <inpo686@gmail.com>
2022-07-26 23:48:46 +0300

    .

--- main.ts
+++ main.ts
@@ -76,3 +76,41 @@ let Spashipe: ICar = {model: "Africa", numOfDoors: 5, numOfWheels:5}
 let Tesla: ICar = {model: "Bali", numOfDoors: 4, numOfWheels:4}
 const BonbONGaradge: Garage = new Garage("Nona", "Mike", false,[Bike,Spashipe,Tesla])
 console.log(BonbONGaradge)
+
+// קלטי 2 מס והדפיסי את כולם מהנמוך לגבוהה 
+
+function numsBetween(minNum: number, maxNum: number):number[]{
+    const betweenArr : number[] = []
+    for(let i = minNum+1; i<maxNum; i++){
+        betweenArr.push(i)
+
+    }
+    return betweenArr
+}
+function printMin(num1: number, num2: number):void{
+    num1 == Math.min(num1,num2) ? console.log(numsBetween(num1,num2)) : console.log(numsBetween(num2,num1))
+}
+// printMin(3,6)
+
+function printSort(...numbers: number[]):void{
+    numbers ? console.log(numbers.sort((a,b) => a-b)) : "no parameters were given"
+}
+// printSort(9,5,2,1)
+
+// הכניסי את כל המספרים הזוגיים  למערך 
+
+function getEven(...numbers: number[]):number[]{
+    let evenArr: number [] = []
+    numbers ? numbers.forEach(item => {item % 2 == 0 ? evenArr.push(item) : console.log( "not even")}) :console.log( "no parameters")
+    return evenArr
+}
+// console.log(getEven(2,5,8,6,4,11,31,20))
+
+function getEvenNum(num:number): number[]{
+    let evenArr: number [] = []
+    for(let i = 0; i<num; i++){
+        i % 2 == 0 ? evenArr.push(i) : console.log("not even")
+    }
+    return evenArr
+}
+console.log(getEvenNum(20))
